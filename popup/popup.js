e29832f45f3a222db64bda7c2c7ffbea7083d989
Naveen1812Rawat <jamesrawat575@gmail.com>
2024-06-10 01:34:27 +0530

    added basic template

---
+++ popup/popup.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('actionButton').addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
